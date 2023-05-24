0ba6db63a44cc012a3266284d190b8daec11050e
AlexaPankek <anadeau.1.2223@tech4maine.org>
2023-05-24 13:35:32 -0400

    Attempting Code Again

---
+++ Darkened Dealer/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenuPanel;
+
+    private bool isPaused;
+
+    private void Start()
+    {
+        ResumeGame();
+    }
+
+    private void Update()
+    {
+        // Check for pause input (e.g., Esc key)
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+           if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                Debug.Log("Escape key pressed");
+            }
+            else
+            {
+                PauseGame();
+                Debug.Log("resume game");
+            }
+        }
+    }
+
+    public void PauseGame()
+    {
+        isPaused = true;
+        Time.timeScale = 0f; // Pause the game
+        pauseMenuPanel.SetActive(true);
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    public void ResumeGame()
+    {
+        isPaused = false;
+        Time.timeScale = 1f; // Resume normal time
+        pauseMenuPanel.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    public void QuitGame()
+    {
+        // Implement your quit game functionality here
+        Debug.Log("Quitting game...");
+        Application.Quit();
+    }
+}
--- Darkened Dealer/Assets/Scripts/PlayerMovement.cs
+++ Darkened Dealer/Assets/Scripts/PlayerMovement.cs
@@ -2,23 +2,37 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f;
+    //Walk, run, and camera sensitivty values
+    public float walkSpeed = 5f;
+    public float runSpeed = 10f;
     public float mouseSensitivity = 2f;
-    public float jumpForce = 5f;
-    public float gravity = 9.8f;
 
+    //Camera, character controller, speed, and rotation values
     private CharacterController characterController;
     private Camera playerCamera;
     private float verticalRotation = 0f;
-    private Vector3 moveDirection;
-    private bool isJumping;
+    private float currentSpeed;
+
+    //Stamina Bar
+    public float maxStamina = 100f;
+    public float staminaConsumptionRate = 10f;
+    public float staminaRegenerationRate = 5f;
+
+    private float currentStamina;
+    private StaminaBar staminaBar;
+
 
     private void Start()
     {
         characterController = GetComponent<CharacterController>();
-        playerCamera = Camera.main;
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
+    playerCamera = Camera.main;
+    Cursor.lockState = CursorLockMode.Locked;
+    Cursor.visible = false;
+    currentSpeed = walkSpeed;
+
+    staminaBar = FindObjectOfType<StaminaBar>();
+    staminaBar.SetMaxStamina(maxStamina);
+    currentStamina = maxStamina;
     }
 
     private void Update()
@@ -40,29 +54,27 @@ public class PlayerMovement : MonoBehaviour
         // Calculate the movement direction based on input
         Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical);
         movement = transform.TransformDirection(movement);
-        movement *= moveSpeed;
-
-        // Apply gravity
-        if (!characterController.isGrounded)
-        {
-            movement.y -= gravity * Time.deltaTime;
-        }
-
-        // Jumping
-        if (characterController.isGrounded)
-        {
-            if (!isJumping && Input.GetButtonDown("Jump"))
-            {
-                movement.y = jumpForce;
-                isJumping = true;
-            }
-        }
-        else
-        {
-            isJumping = false;
-        }
+        movement *= currentSpeed;
 
         // Apply movement to the character controller
         characterController.Move(movement * Time.deltaTime);
+
+        // Check for sprinting
+    if (Input.GetKey(KeyCode.LeftShift) && currentStamina > 0)
+    {
+        currentSpeed = runSpeed;
+        currentStamina -= staminaConsumptionRate * Time.deltaTime;
+        staminaBar.SetStamina(currentStamina);
+    }
+    else
+    {
+        currentSpeed = walkSpeed;
+        if (currentStamina < maxStamina)
+        {
+            currentStamina += staminaRegenerationRate * Time.deltaTime;
+            staminaBar.SetStamina(currentStamina);
+        }
+    }
+    currentStamina = Mathf.Clamp(currentStamina, 0f, maxStamina);
     }
 }
